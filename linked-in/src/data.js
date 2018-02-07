a1569c436cb18442ce76d3563d8bfd2bfd13a83e
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-07 17:55:32 -0600

    change dummy images to actual ones

--- linked-in/src/data.js
+++ linked-in/src/data.js
@@ -1,104 +1,104 @@
 const Data = [{
   "id": 1,
-  "image": "http://dummyimage.com/100x100.bmp/dddddd/000000",
+  "image": "https://media-cdn.tripadvisor.com/media/photo-s/05/bb/b2/2e/wild-life-sancyuary-wayanad.jpg",
   "fullName": "Dynah Yarnton",
   "gender": "Female",
   "commonName": "Squirrel, malabar",
   "city": "Maştağa"
 }, {
   "id": 2,
-  "image": "http://dummyimage.com/100x100.jpg/ff4444/ffffff",
+  "image": "http://snake-facts.weebly.com/uploads/6/5/5/3/6553869/2514638.jpg",
   "fullName": "Clifford Atmore",
   "gender": "Male",
   "commonName": "Snake, tiger",
   "city": "Mtwango"
 }, {
   "id": 3,
-  "image": "http://dummyimage.com/100x100.bmp/5fa2dd/ffffff",
+  "image": "http://www.biodiversityexplorer.org/birds/pycnonotidae/images/72586635.Vw13OKsG_327w.jpg",
   "fullName": "Abbi Summerley",
   "gender": "Female",
   "commonName": "Bulbul, black-eyed",
   "city": "Nora"
 }, {
   "id": 4,
-  "image": "http://dummyimage.com/100x100.bmp/cc0000/ffffff",
+  "image": "http://www.nationalpedia.com/wp-content/uploads/2017/01/picturev-of-Great-White-Pelican-300x213.jpg",
   "fullName": "Thorstein Westmancoat",
   "gender": "Male",
   "commonName": "Pelican, great white",
   "city": "Acherítou"
 }, {
   "id": 5,
-  "image": "http://dummyimage.com/100x100.jpg/dddddd/000000",
+  "image": "https://tranquilityreptiles.files.wordpress.com/2011/09/23-8-11-107-copy.jpg",
   "fullName": "Winifred Dies",
   "gender": "Female",
   "commonName": "Cook's tree boa",
   "city": "Phoenix"
 }, {
   "id": 6,
-  "image": "http://dummyimage.com/100x100.jpg/ff4444/ffffff",
+  "image": "http://www.photodestination.co.za/media/reviewsphotos/original/0c/7e/ba/the-tsitsikamma-marine-protected-area-44-1414210260.jpg",
   "fullName": "Enrika Wylam",
   "gender": "Female",
   "commonName": "Otter, cape clawless",
   "city": "Kudus"
 }, {
   "id": 7,
-  "image": "http://dummyimage.com/100x100.bmp/5fa2dd/ffffff",
+  "image": "https://gurumavin.com/wp-content/uploads/2014/12/barasinga0509-400x330.jpg",
   "fullName": "Filippo Roan",
   "gender": "Male",
   "commonName": "Swamp deer",
   "city": "Yahil’nytsya"
 }, {
   "id": 8,
-  "image": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff",
+  "image": "https://www.portfoliocollection.com/img/uploads/blogs/black%20footed.jpg",
   "fullName": "Joeann Arnet",
   "gender": "Female",
   "commonName": "Cape wild cat",
   "city": "Velas"
 }, {
   "id": 9,
-  "image": "http://dummyimage.com/100x100.png/5fa2dd/ffffff",
+  "image": "http://www.bushpea.com/bd/ph/laz/mi/little%20pied%20cormorant%2005.jpg",
   "fullName": "Manda Mabon",
   "gender": "Female",
   "commonName": "Pied cormorant",
   "city": "Bolboc"
 }, {
   "id": 10,
-  "image": "http://dummyimage.com/100x100.png/5fa2dd/ffffff",
+  "image": "http://www.animalspot.net/wp-content/uploads/2011/09/Scarlet-Macaw-Images.jpg",
   "fullName": "Isidor Colliard",
   "gender": "Male",
   "commonName": "Scarlet macaw",
   "city": "Mikhaylovka"
 }, {
   "id": 11,
-  "image": "http://dummyimage.com/100x100.bmp/5fa2dd/ffffff",
+  "image": "https://www.petinfoclub.com/Images/Blue-breasted%20waxbill%20shutterstock_266270099.jpg",
   "fullName": "Pollyanna Mawson",
   "gender": "Female",
   "commonName": "Waxbill, blue",
   "city": "Tamanan"
 }, {
   "id": 12,
-  "image": "http://dummyimage.com/100x100.png/ff4444/ffffff",
+  "image": "https://www.allaboutbirds.org/guide/PHOTO/LARGE/Maskedbooby-calebslemmons.JPG",
   "fullName": "Jackie Bader",
   "gender": "Male",
   "commonName": "Masked booby",
   "city": "Asamboka"
 }, {
   "id": 13,
-  "image": "http://dummyimage.com/100x100.bmp/ff4444/ffffff",
+  "image": "http://www.ourendangeredworld.com/wp-content/uploads/2013/05/Oriental-small-clawed-otter-1.jpg",
   "fullName": "Benedict Kennealy",
   "gender": "Male",
   "commonName": "Small-clawed otter",
   "city": "Sabinópolis"
 }, {
   "id": 14,
-  "image": "http://dummyimage.com/100x100.png/5fa2dd/ffffff",
+  "image": "http://www.factzoo.com/sites/all/img/reptiles/turtle/indian-star-tortoise-walk.jpg",
   "fullName": "Basilio Fishwick",
   "gender": "Male",
   "commonName": "Indian star tortoise",
   "city": "Horodyshche"
 }, {
   "id": 15,
-  "image": "http://dummyimage.com/100x100.png/dddddd/000000",
+  "image": "http://blog.martinbelan.com/wp-content/uploads/2012/07/Yellowstone_Uinta_Ground_Squirrel_061312_0002.jpg",
   "fullName": "Martie Shilton",
   "gender": "Female",
   "commonName": "Uinta ground squirrel",
