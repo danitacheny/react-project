57ca61f5502138fb0ba05c6e2a6f9985a4b9ff84
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-07 17:26:52 -0600

    style profile cards
    ProfileCard.js: switched order of city and species
    
    ProfileCard.css:
    - centers the information within each card
    - lines 11-12 add color change when you hover over the card
    - lines 53-57 add color change when you hover over the button

--- linked-in/src/components/ProfileCard/ProfileCard.js
+++ linked-in/src/components/ProfileCard/ProfileCard.js
@@ -7,8 +7,8 @@ const ProfileCard = (props) => {
       <div className="card__info">
         <img className="card__img" src={props.profile.image} alt="profile pic" />
         <div className="card__name">{props.profile.fullName}</div>
-        <div className="card__species">{props.profile.commonName}</div>
         <div className="card__city">{props.profile.city}</div>
+        <div className="card__species">{props.profile.commonName}</div>
       </div>
       <div className="card__button">Connect</div>
     </div>
