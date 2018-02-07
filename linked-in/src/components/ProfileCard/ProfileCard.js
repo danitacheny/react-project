2f60df69f1348cb656128eee8cbadfaa0975a256
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-07 16:19:37 -0600

    add ProfileCard component and style

---
+++ linked-in/src/components/ProfileCard/ProfileCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ProfileCard.css';
+
+class ProfileCard extends Component {
+  state = {
+
+  }
+
+  render = () => {
+    <div className="ProfileCard">
+      
+    </div>
+  }
+}
