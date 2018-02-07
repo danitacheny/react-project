7db7e735d5c2a2de474096908bce1f24bb623cbb
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-07 16:14:04 -0600

    add NavBar component to App.js

--- linked-in/src/App.js
+++ linked-in/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+// import Data from './data.js';
+import NavBar from './components/NavBar/NavBar.js';
 
 class App extends Component {
   render = () => {
     return (
       <div>
+        <div>
+          <NavBar />
+        </div>
       </div>
     )
   }
