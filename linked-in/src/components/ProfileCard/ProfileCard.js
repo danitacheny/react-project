565fb89de6d87b0b06cc751bcfbabf2c18ef2e50
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-07 18:56:00 -0600

    minor changes
    ProfileCard.js:
    - added a company line for the animals to show
    off how fancy they are with their jobs.
    - Clarifies that the second line in the card is actually a
    location (it's a city)
    
    ProfileCard.css: makes the card a bit bigger to accomodate
    longer text
    
    data.js: change gender to company (gender was an accident)
    
    App.css adds padding underneath the profile list as well as
    the top

--- linked-in/src/components/ProfileCard/ProfileCard.js
+++ linked-in/src/components/ProfileCard/ProfileCard.js
@@ -7,8 +7,9 @@ const ProfileCard = (props) => {
       <div className="card__info">
         <img className="card__img" src={props.profile.image} alt="profile pic" />
         <div className="card__name">{props.profile.fullName}</div>
-        <div className="card__city">{props.profile.city}</div>
+        <div className="card__city">Lives in {props.profile.city}</div>
         <div className="card__species">{props.profile.commonName}</div>
+        <div className="card__company">{props.profile.company}</div>
       </div>
       <div className="card__button">Connect</div>
     </div>
--- linked-in/src/data.js
+++ linked-in/src/data.js
@@ -2,105 +2,105 @@ const Data = [{
   "id": 1,
   "image": "https://media-cdn.tripadvisor.com/media/photo-s/05/bb/b2/2e/wild-life-sancyuary-wayanad.jpg",
   "fullName": "Dynah Yarnton",
-  "gender": "Female",
+  "company": "Vimbo",
   "commonName": "Squirrel, malabar",
   "city": "Maştağa"
 }, {
   "id": 2,
   "image": "http://snake-facts.weebly.com/uploads/6/5/5/3/6553869/2514638.jpg",
   "fullName": "Clifford Atmore",
-  "gender": "Male",
+  "company": "Podcat",
   "commonName": "Snake, tiger",
   "city": "Mtwango"
 }, {
   "id": 3,
   "image": "http://www.biodiversityexplorer.org/birds/pycnonotidae/images/72586635.Vw13OKsG_327w.jpg",
   "fullName": "Abbi Summerley",
-  "gender": "Female",
+  "company": "Twinder",
   "commonName": "Bulbul, black-eyed",
   "city": "Nora"
 }, {
   "id": 4,
   "image": "http://www.nationalpedia.com/wp-content/uploads/2017/01/picturev-of-Great-White-Pelican-300x213.jpg",
   "fullName": "Thorstein Westmancoat",
-  "gender": "Male",
+  "company": "Brightbean",
   "commonName": "Pelican, great white",
   "city": "Acherítou"
 }, {
   "id": 5,
   "image": "https://tranquilityreptiles.files.wordpress.com/2011/09/23-8-11-107-copy.jpg",
   "fullName": "Winifred Dies",
-  "gender": "Female",
+  "company": "Twitternation",
   "commonName": "Cook's tree boa",
   "city": "Phoenix"
 }, {
   "id": 6,
   "image": "http://www.photodestination.co.za/media/reviewsphotos/original/0c/7e/ba/the-tsitsikamma-marine-protected-area-44-1414210260.jpg",
   "fullName": "Enrika Wylam",
-  "gender": "Female",
+  "company": "Skinix",
   "commonName": "Otter, cape clawless",
   "city": "Kudus"
 }, {
   "id": 7,
   "image": "https://gurumavin.com/wp-content/uploads/2014/12/barasinga0509-400x330.jpg",
   "fullName": "Filippo Roan",
-  "gender": "Male",
+  "company": "Wordware",
   "commonName": "Swamp deer",
   "city": "Yahil’nytsya"
 }, {
   "id": 8,
   "image": "https://www.portfoliocollection.com/img/uploads/blogs/black%20footed.jpg",
   "fullName": "Joeann Arnet",
-  "gender": "Female",
+  "company": "Gigabox",
   "commonName": "Cape wild cat",
   "city": "Velas"
 }, {
   "id": 9,
   "image": "http://www.bushpea.com/bd/ph/laz/mi/little%20pied%20cormorant%2005.jpg",
   "fullName": "Manda Mabon",
-  "gender": "Female",
+  "company": "Riffpath",
   "commonName": "Pied cormorant",
   "city": "Bolboc"
 }, {
   "id": 10,
   "image": "http://www.animalspot.net/wp-content/uploads/2011/09/Scarlet-Macaw-Images.jpg",
   "fullName": "Isidor Colliard",
-  "gender": "Male",
+  "company": "Tavu",
   "commonName": "Scarlet macaw",
   "city": "Mikhaylovka"
 }, {
   "id": 11,
   "image": "https://www.petinfoclub.com/Images/Blue-breasted%20waxbill%20shutterstock_266270099.jpg",
   "fullName": "Pollyanna Mawson",
-  "gender": "Female",
+  "company": "Skyble",
   "commonName": "Waxbill, blue",
   "city": "Tamanan"
 }, {
   "id": 12,
   "image": "https://www.allaboutbirds.org/guide/PHOTO/LARGE/Maskedbooby-calebslemmons.JPG",
   "fullName": "Jackie Bader",
-  "gender": "Male",
+  "company": "Geba",
   "commonName": "Masked booby",
   "city": "Asamboka"
 }, {
   "id": 13,
   "image": "http://www.ourendangeredworld.com/wp-content/uploads/2013/05/Oriental-small-clawed-otter-1.jpg",
   "fullName": "Benedict Kennealy",
-  "gender": "Male",
+  "company": "Skyba",
   "commonName": "Small-clawed otter",
   "city": "Sabinópolis"
 }, {
   "id": 14,
   "image": "http://www.factzoo.com/sites/all/img/reptiles/turtle/indian-star-tortoise-walk.jpg",
   "fullName": "Basilio Fishwick",
-  "gender": "Male",
+  "company": "Reallinks",
   "commonName": "Indian star tortoise",
   "city": "Horodyshche"
 }, {
   "id": 15,
   "image": "http://blog.martinbelan.com/wp-content/uploads/2012/07/Yellowstone_Uinta_Ground_Squirrel_061312_0002.jpg",
   "fullName": "Martie Shilton",
-  "gender": "Female",
+  "company": "Dabvine",
   "commonName": "Uinta ground squirrel",
   "city": "Banjar Kampungbugis"
 }]
