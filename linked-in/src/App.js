ba3bfbf94cb67b492a413752ca8291a760baa115
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-08 16:05:42 -0600

    add connections box
    App.js added Connections box and main-content container
    
    App.css adds styling for main-content so connections and profiles are next
    to each other on the screen

--- linked-in/src/App.js
+++ linked-in/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Data from './data.js';
 import NavBar from './components/NavBar/NavBar.js';
 import ProfileCard from './components/ProfileCard/ProfileCard.js';
+import Connections from './components/Connections/Connections.js';
 import './App.css';
 
 class App extends Component {
@@ -11,17 +12,22 @@ class App extends Component {
         <div>
           <NavBar />
         </div>
-        <div className="profiles__container">
-          <div className="profiles">
-            {Data.map((profile) => {
-              return (
-                <div>
-                  <ProfileCard key={profile.id} profile={profile} />
-                </div>
-              )
-            })}
+        <div className="main-content">
+          <div className="connections__container">
+            <Connections />
           </div>
+          <div className="profiles__container">
+            <div className="profiles">
+              {Data.map((profile) => {
+                return (
+                  <div>
+                    <ProfileCard key={profile.id} profile={profile} />
+                  </div>
+                )
+              })}
+            </div>
           </div>
+        </div>
       </div>
     )
   }
